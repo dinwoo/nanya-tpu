4805513e79f4a97c331a748a094cb120c94d8b22
dinwoo <danielforboy@gmail.com>
2022-05-22 23:47:07 +0800

    Update 2022-05-22T15:46:51.436Z

--- js/all.js
+++ js/all.js
@@ -1,2 +1,2 @@
-"use strict";var fun=function(){},fun2=(fun(),function(){});fun2();
+"use strict";var home=function(){$(".slider").slick({infinite:!0,dots:!0,arrows:!1,autoplay:!0})},fun2=function(){};fun2();
 //# sourceMappingURL=all.js.map

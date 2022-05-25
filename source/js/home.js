415276bc5d43a2823ffcccff0fbc53fe96f8295f
dinwoo <danielforboy@gmail.com>
2022-05-26 00:06:10 +0800

    feat: 首頁切版

--- source/js/home.js
+++ source/js/home.js
@@ -5,4 +5,35 @@ let home = () => {
     arrows: false,
     autoplay: true,
   });
+  $(".product-main-carousel").slick({
+    infinite: true,
+    dots: false,
+    arrows: false,
+    autoplay: true,
+    slidesToShow: 1,
+    asNavFor: $(".product-dot-carousel"),
+  });
+  $(".product-dot-carousel").slick({
+    infinite: true,
+    dots: false,
+    arrows: false,
+    autoplay: true,
+    slidesToShow: 7,
+    centerMode: true,
+    asNavFor: $(".product-main-carousel"),
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+    ],
+  });
+  $("#product-pre").on("click", () => {
+    $(".product-dot-carousel").slick("slickPrev");
+  });
+  $("#product-next").on("click", () => {
+    $(".product-dot-carousel").slick("slickNext");
+  });
 };
